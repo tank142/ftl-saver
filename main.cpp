aa84b75585fd16bf4bf79ccb5656ccf132940437
tank142 <tank.142@protonmail.com>
2021-03-09 15:14:52 +0400

    ...

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include <QApplication>
 #include <QTranslator>
 #include <QLibraryInfo>
-
+#include <QStandardPaths>
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	QTranslator translator;
-	if(translator.load("ftl-saver_ru_RU","/home/tank/ftl-saver/ftl-saver/")){
-		translator.load("ftl-saver_en_US","/home/tank/ftl-saver/ftl-saver/");
+	if(translator.load("ftl-saver_ru_RU",QStandardPaths::standardLocations(QStandardPaths::GenericDataLocation).at(2))){
+		translator.load("ftl-saver_en_US",QStandardPaths::standardLocations(QStandardPaths::GenericDataLocation).at(2));
 	}
 	a.installTranslator(&translator);
 	MainWindow w;
